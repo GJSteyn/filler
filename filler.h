127866ed7796fda5943e66e4e5b083ee97981eca
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-06-11 20:36:03 +0200

    Testing input from filler_vm

--- filler.h
+++ filler.h
@@ -2,6 +2,12 @@
 # define FILLER_H
 
 # include "./libft/libft.h"
+# include "./get_next_line/get_next_line.h"
 # include <unistd.h>
+
+typedef struct		s_player
+{
+	char		c;
+}			t_player;
 
 #endif
--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 
 int	main(void)
 {
+	int		ret;
+	char		*parse;
+	static t_player	*player;
 
+	if (!player)
+		player = (t_player*)malloc(sizeof(t_player));
+	ret = get_next_line(1, &parse);
+	player->c = parse[11];
+	write(2, &(player->c), 1);
+	write(2, "\n", 1);
+	ft_putnbr(ret);
+	free(parse);
+	free(player);
 	return (0);
 }
