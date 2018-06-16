723ea4ccce4bbf352fe89e989e613a1af5cc1ab7
GJSteyn <6443537+GJSteyn@users.noreply.github.com>
2018-06-16 17:10:15 +0200

    Input working

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/15 09:24:16 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/06/15 12:04:09 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/06/16 16:04:48 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,17 @@ int	main(void)
 		player = (t_player*)malloc(sizeof(t_player));
 		ret = get_next_line(1, &parse);
 		player->c = ft_get_player_nr(parse[10]);
+		while ((ret = get_next_line(0, &parse)))
+		{
+			ft_putstr_fd(parse, 2);
+		}
 	}
 	else
 	{
-		ret = get_next_line(1, &parse);
+		while ((ret = get_next_line(0, &parse)))
+		{
+			ft_putstr_fd(parse, 2);
+		}
 	}
 	write(2, parse, 5);
 	//write(2, "\n", 1);
