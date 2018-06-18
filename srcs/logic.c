b334c130e516bf75519c59bcca76c0fc2f42a814
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2018-06-18 19:43:19 +0200

    Backup small changes. No real progress

--- srcs/logic.c
+++ srcs/logic.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 08:30:24 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/06/18 13:57:04 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/06/18 16:33:44 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int			ft_itop(int x, int y, t_2dvect dim)
 {
-	return ((x * (dim.x - 1)) + y);
+	return ((x * dim.y) + y);
 }
 
 t_2dvect	ft_itovect(int x, int y)
@@ -36,9 +36,9 @@ int			ft_valid_pos(t_piece *piece, t_2dvect pos, t_filler *fill)
 	i = 0;
 	j = 0;
 	count = 0;
-	while (i < piece->dim.y)
+	while (i < piece->dim.x)
 	{
-		while (j < piece->dim.x)
+		while (j < piece->dim.y)
 		{
 			map_piece = fill->map->grid[ft_itop(i + pos.x, j + pos.y, fill->map->dim)];
 			if (piece->grid[ft_itop(i, j, piece->dim)] == '*' && (map_piece == fill->player->c || map_piece == fill->player->c - 32))
@@ -60,8 +60,8 @@ char		*ft_ind_to_str(int x, int y)
 	char	*str2;
 	char	*temp;
 
-	str1 = ft_itoa(y);
-	str2 = ft_itoa(x);
+	str1 = ft_itoa(x);
+	str2 = ft_itoa(y);
 	temp = ft_strjoin(str1, " ");
 	ft_strdel(&str1);
 	str1 = ft_strjoin(temp, str2);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gsteyn <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/15 09:24:16 by gsteyn            #+#    #+#             */
-/*   Updated: 2018/06/18 13:57:06 by gsteyn           ###   ########.fr       */
+/*   Updated: 2018/06/18 18:55:41 by gsteyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,29 @@ void		ft_filler(t_filler *fill)
 
 	i = 0;
 	j = 0;
-	ft_putstr_fd("Inside filler loop\n", 2);
-	ret = get_next_line(0, &parse);		// get line with map size
-	ft_get_map_dim(fill->map, parse);
+	//ft_putstr_fd("Inside filler loop\n", 2);
+	if (!(fill->map->dim.x > 0))
+	{
+		ret = get_next_line(0, &parse);		// get line with map size
+		ft_get_map_dim(fill->map, parse);
+	}
+	else
+		ret = get_next_line(0, &parse);
+	if (ret == 0)
+	{
+		ft_putstr_fd("nopegnl\n", 2);
+		exit(1);
+	}
+	/*
+	if (ret >0)
+	{
+		ft_get_map_dim(fill->map, parse);
+		ft_putstr_fd("this: ", 2);
+		ft_putstr_fd(parse, 2);
+	}*/
 	ft_get_map(fill->map);
 	ft_get_piece(fill->piece);
-	ft_putstr_fd("After trying to get all the parts\n", 2);
+	//ft_putstr_fd("After trying to get all the parts\n", 2);
 	while (i < fill->map->dim.x - fill->piece->dim.x)
 	{
 		while (j < fill->map->dim.y - fill->piece->dim.y)
@@ -36,6 +53,8 @@ void		ft_filler(t_filler *fill)
 			{
 				temp = ft_ind_to_str(i, j);
 				ft_putstr_fd(temp, 1);
+				//ft_putchar_fd('\n', 2);
+				//ft_putstr_fd(temp, 2);
 				ft_strdel(&temp);
 				return;
 			}
@@ -44,6 +63,12 @@ void		ft_filler(t_filler *fill)
 		j = 0;
 		i++;
 	}
+	//ft_putstr_fd("Before bottom\n", 2);
+	temp = ft_ind_to_str(0, 0);
+	ft_putstr_fd(temp, 1);
+	ft_strdel(&temp);
+	/*ft_putstr_fd(parse, 2);
+	ft_putchar_fd('\n', 2);*/
 }
 
 int		main(void)
@@ -52,7 +77,7 @@ int		main(void)
 	char			*parse;
 	t_filler		*fill;
 
-	ft_putstr_fd("nope\n", 2);
+	//ft_putstr_fd("nope\n", 2);
 	fill = (t_filler*)malloc(sizeof(t_filler));
 	if (!fill)
 		ft_putstr_fd("fill mallerror\n", 2);
@@ -66,14 +91,18 @@ int		main(void)
 	if (!fill->piece)
 		ft_putstr_fd("piece mallerror\n", 2);
 	ret = get_next_line(0, &parse);		// get line with player number
+	if (ret <= 0)
+	{
+		ft_putstr_fd("nopegnlmain\n", 2);
+		exit(1);
+	}
 	if (parse[10] == '1')
 		fill->player->c = 'o';
 	else if (parse[10] == '2')
 		fill->player->c = 'x';
 	else
 		ft_putstr_fd("player number error\n", 2);
-	//ft_putstr("19 18\n");
-	//while (1)
-	ft_filler(fill);
+	while (1)
+		ft_filler(fill);
 	return (0);
 }
